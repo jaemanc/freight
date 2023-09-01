2e943f1add7c30fb400ad6625f961edd516d6013
jaemanc <jaemanc93@gmail.com>
2023-09-01 20:57:37 +0900

    Initial commit

---
+++ src/main/java/com/express/freight/FreightApplication.java
@@ -0,0 +1,13 @@
+package com.express.freight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/express/freight/FreightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.freight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
