0c4ddd33aaf28c6eb2d2c8c4a7cc07c7c9f7b44b
jaemanc <jaemanc93@gmail.com>
2023-09-04 11:46:19 +0900

    * main
    1. swagger 세팅.
    2. SQLite3 DB 세팅.

--- src/main/java/com/express/freight/maintenance/MaintenanceController.java
+++ src/main/java/com/express/freight/maintenance/MaintenanceController.java
@@ -1,4 +1,25 @@
 package com.express.freight.maintenance;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MaintenanceController {
+
+    @Operation(summary = "operation summary test", description = "operation description test !!")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK!!"),
+            @ApiResponse(responseCode = "400", description = "BAD!!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND!!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR"),
+    })
+    @GetMapping("/hello")
+    public ResponseEntity<String> temp(){
+        return ResponseEntity.ok(" hello world? ");
+    }
+
 }
--- src/main/java/com/express/freight/user/dto/UserEntity.java
+++ src/main/java/com/express/freight/user/dto/UserEntity.java
@@ -1,4 +1,24 @@
 package com.express.freight.user.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
 public class UserEntity {
+    @Id
+    private Long user_id;
+
+    private String name;
+
+    private String contact;
+
+    private String email;
+
+    private String extra;
+
+
 }
